10c7fdba5a7411343dcdb9b62a6e296d87622be9
root <root@LAPTOP-2IHH1T5U.localdomain>
2026-02-22 22:09:43 +0530

    Updated custom-workflow

--- custom-workflow/src/index.ts
+++ custom-workflow/src/index.ts
@@ -1,1 +1,12 @@
+import fs from "fs";
+
 console.log("Start");
+
+ fs.readdir("./", { recursive: true, withFileTypes: true }, (err, files) => {
+   if (err) console.log("Error occured reading file");
+   files.forEach((fileEntry) => {
+     console.log("Name: " + fileEntry.name, "Path : " + fileEntry.parentPath);
+   });
+ });
+
+
