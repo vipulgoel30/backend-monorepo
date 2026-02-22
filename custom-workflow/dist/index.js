af429f732c3f5fdeb518b63bb4897891e7492978
root <root@LAPTOP-2IHH1T5U.localdomain>
2026-02-22 22:12:26 +0530

    Updated dist for custom-workflow

--- custom-workflow/dist/index.js
+++ custom-workflow/dist/index.js
@@ -1,2 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
 console.log("Start");
+fs_1.default.readdir("./", { recursive: true, withFileTypes: true }, (err, files) => {
+    if (err)
+        console.log("Error occured reading file");
+    files.forEach((fileEntry) => {
+        console.log("Name: " + fileEntry.name, "Path : " + fileEntry.parentPath);
+    });
+});
